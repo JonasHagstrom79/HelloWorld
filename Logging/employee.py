fb6bd012145ab464250ebd16a050d4d4c5451205
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-09-29 13:58:03 +0200

    Added logging to each class

--- Logging/employee.py
+++ Logging/employee.py
@@ -1,7 +1,25 @@
 import logging
 
-logging.basicConfig(filename='employee.log', level=logging.INFO,
-                    format='%(levelname)s:%(message)s')
+# To make different loggers and not mess up root because the first initiated logger sets the level
+# Thats not good if you need to use several different in the same function
+logger = logging.getLogger(__name__)
+# 5 Set log lever on logger
+logger.setLevel(logging.INFO)
+
+# 3 Add formating to file_handler, not the logger. Keep that in mind
+formatter = logging.Formatter('%(levelname)s:%(message)s')
+
+# 1 Create filehandler to be able to add to logger above
+file_handler = logging.FileHandler('employee.log')
+# 4 Add formatter to file_handler
+file_handler.setFormatter(formatter)
+
+# 2 Add file_handler to logger
+logger.addHandler(file_handler)
+
+# Root logger, not needed after the config above
+#logging.basicConfig(filename='employee.log', level=logging.INFO,
+#                    format='%(levelname)s:%(message)s')
 
 
 class Employee:
@@ -12,7 +30,8 @@ class Employee:
         self.last = last
 
         #print("Created Employee: {} {}".format(self.fullname, self.email))
-        logging.info("Created Employee: {} {}".format(self.fullname, self.email))
+        #logging.info("Created Employee: {} {}".format(self.fullname, self.email))
+        logger.info("Created Employee: {} {}".format(self.fullname, self.email)) # From logger variable at the top
 
     @property
     def email(self):
--- Logging/log-sample.py
+++ Logging/log-sample.py
@@ -1,4 +1,6 @@
 import logging
+# Import emploee class with its own logger(will be run at import)
+import employee
 
 # DEBUG: Detailed information, typically of interest only when diagnosing problems.
 
@@ -10,9 +12,33 @@ import logging
 
 # CRITICAL: A serious error, indicating that the program itself may be unable to continue running.
 
-logging.basicConfig(filename='test.log', level=logging.DEBUG,
-                    format='%(asctime)s:%(levelname)s:%(message)s')
+logger = logging.getLogger(__name__)
+# 5 Set log lever on logger
+logger.setLevel(logging.DEBUG)
 
+# 3 Add formating to file_handler, not the logger. Keep that in mind
+formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+
+# 1 Create filehandler to be able to add to logger above
+file_handler = logging.FileHandler('test.log')
+# 6 Set differnet logging properties on the file_handeler
+file_handler.setLevel(logging.ERROR) # Also change the method division further down
+# 4 Add formatter to file_handler
+file_handler.setFormatter(formatter)
+
+# 7 Create a stream_handeler for console output
+stream_handler = logging.StreamHandler()
+# 9 Set formatter to stream_handeler
+stream_handler.setFormatter(formatter) # Can create a completely new if we want to
+
+# 2 Add file_handler to logger
+logger.addHandler(file_handler)
+# 8 Add stream_handler to logger
+logger.addHandler(stream_handler)
+
+# No need for the root config anymore
+#logging.basicConfig(filename='test.log', level=logging.DEBUG,
+#                    format='%(asctime)s:%(levelname)s:%(message)s')
 
 def add(x, y):
     """Add Function"""
@@ -31,14 +57,22 @@ def multiply(x, y):
 
 def divide(x, y):
     """Divide Function"""
-    return x / y
+    try:
+        result = x / y
+    except ZeroDivisionError:
+        # Change to exception(from error) to be able to trace the error
+        logger.exception("Tried to divide by zero")
+    else:
+        return result
+
 
 
 num_1 = 20
-num_2 = 10
+num_2 = 0 # To get the error message
 
 add_result = add(num_1, num_2)
-logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+logger.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
+#logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 #logging.info('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 #logging.warning('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 #logging.error('Add: {} + {} = {}'.format(num_1, num_2, add_result))
@@ -46,7 +80,8 @@ logging.debug('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 #print('Add: {} + {} = {}'.format(num_1, num_2, add_result))
 
 sub_result = subtract(num_1, num_2)
-logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+logger.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
+#logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 #logging.info('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 #logging.warning('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 #logging.error('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
@@ -54,7 +89,8 @@ logging.debug('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 #print('Sub: {} - {} = {}'.format(num_1, num_2, sub_result))
 
 mul_result = multiply(num_1, num_2)
-logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+logger.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
+#logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 #logging.info('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 #logging.warning('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 #logging.error('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
@@ -62,7 +98,8 @@ logging.debug('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 #print('Mul: {} * {} = {}'.format(num_1, num_2, mul_result))
 
 div_result = divide(num_1, num_2)
-logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+logger.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
+#logging.debug('Div: {} / {} = {}'.format(num_1, num_2, div_result))
 #logging.info('Div: {} / {} = {}'.format(num_1, num_2, div_result))
 #logging.warning('Div: {} / {} = {}'.format(num_1, num_2, div_result))
 #logging.error('Div: {} / {} = {}'.format(num_1, num_2, div_result))
