98b953c34095a9d1bb387268d1a6c9793ab71975
JonasHagstrom79 <73818160+JonasHagstrom79@users.noreply.github.com>
2022-09-29 10:42:25 +0200

    Added logging to employee

--- Logging/employee.py
+++ Logging/employee.py
@@ -1,3 +1,9 @@
+import logging
+
+logging.basicConfig(filename='employee.log', level=logging.INFO,
+                    format='%(levelname)s:%(message)s')
+
+
 class Employee:
     """A sample Employee class"""
 
@@ -5,15 +11,21 @@ class Employee:
         self.first = first
         self.last = last
 
-        print("Created Employee: {} {}").format(self.fullname, self.email)
+        #print("Created Employee: {} {}".format(self.fullname, self.email))
+        logging.info("Created Employee: {} {}".format(self.fullname, self.email))
 
     @property
-    def email(selfself):
-        return"{}.{}@email.com".format(self.first, self.last)
+    def email(self):
+        return "{}.{}@email.com".format(self.first, self.last)
 
     @property
     def fullname(self):
         return "{} {}".format(self.first, self.last)
 
 emp_1 = Employee("John", "Doe")
-emp_1 = Employee("Corey", "Schafer")
+emp_2 = Employee("Corey", "Schafer")
+emp_3 = Employee("Tony", "Stark")
+emp_4 = Employee("Peter", "Parker")
+emp_5 = Employee("Bruce", "Wayne")
+emp_6 = Employee("Jane", "Doe")
+emp_7 = Employee("Clark", "Kent")
